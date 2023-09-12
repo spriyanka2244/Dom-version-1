fdd5f14b34d9201ac76ac0373fc71977e9eea1e3
spriyanka2244 <spriyanka2244@gmail.com>
2023-09-12 14:13:47 +0530

    getElementById

---
+++ index.js
@@ -0,0 +1,34 @@
+// //examin the document object 
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title=123
+// console.log(document.doctype);
+//console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+//  console.log(document.all[10]);
+//  document.all[10].textContent='Hello';
+//sconsole.log(document.forms[0]);
+//console.log(document.links);
+// console.log(document.images);
+
+
+
+
+// getElementById 
+// console.log(document.getElementById('header-title'));
+// var header=document.getElementById('header-title');
+// console.log(header)
+// textcontent
+// header.textContent="welcome"
+// innerText
+// header.innerText="lcome"
+// innerHtml
+// header.innerHTML="<h4>come</h4>"
+// console.log(header);
+
+// style border
+// var headerMain =document.getElementById("main-header")
+// headerMain.style.borderBottom ="solid 3px #000"
+
