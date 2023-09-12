6023b5d42fae4373e44e5cd082b096608adfc0cf
spriyanka2244 <spriyanka2244@gmail.com>
2023-09-12 13:24:01 +0530

    manipulating dom

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log("i love git")
-console.log("I hate git")
-console.log("i kill you")
+// //examin the document object 
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title=123
+// console.log(document.doctype);
+//console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+//  console.log(document.all[10]);
+//  document.all[10].textContent='Hello';
+//sconsole.log(document.forms[0]);
+//console.log(document.links);
+// console.log(document.images);
