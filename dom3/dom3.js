5093e6a6df6e98444e8beb04b883b86e2e83582a
spriyanka2244 <spriyanka2244@gmail.com>
2023-10-13 13:08:30 +0530

    e.target

--- dom3/dom3.js
+++ dom3/dom3.js
@@ -2,8 +2,17 @@
 var button = document.getElementById('button').addEventListener
 ('click',buttonClick);
 
-function buttonClick(){
+function buttonClick(e){
     //console.log('button clicked')
-    document.getElementById('header-title').textContent="hello world"
-    document.querySelector('#main').style.backgroundColor="red"
+    // document.getElementById('header-title').textContent="hello world"
+    // document.querySelector('#main').style.backgroundColor="red"
+  
+    // target    
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.id);
+    console.log(e.target.className);
+    // gives array of dom taken list 
+    console.log(e.target.classList);  
+    
 }
