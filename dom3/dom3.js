c72e21f58c9a08c3678d090ce0a833090c13ffc5
spriyanka2244 <spriyanka2244@gmail.com>
2023-10-13 13:22:42 +0530

    position the element

--- dom3/dom3.js
+++ dom3/dom3.js
@@ -8,15 +8,24 @@ function buttonClick(e){
     // document.querySelector('#main').style.backgroundColor="red"
   
     // target    
-    console.log(e);
-    console.log(e.target);
-    console.log(e.target.id);
-    console.log(e.target.className);
-    // gives array of dom taken list 
-    console.log(e.target.classList);  
-    // output the stuff
-    var output=document.getElementById('output');
-    output.innerHTML='<h3>' +e.target.id+ '</h3>';
-
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(e.target.id);
+    // console.log(e.target.className);
+    // // gives array of dom taken list 
+    // console.log(e.target.classList);  
+    // // output the stuff
+    // var output=document.getElementById('output');
+    // output.innerHTML='<h3>' +e.target.id+ '</h3>';
     
+    //   type of event
+    // console.log(e.type);
+
+    // mouse event 
+    // window actual position  by x and y axis
+    console.log(e.clientX);
+    console.log(e.clientY);
+    // actual element position 
+    console.log(e.offsetX);
+    console.log(e.offsetY);
 }
