919373858464be0fa56104c49b3a6f84eb180d52
spriyanka2244 <spriyanka2244@gmail.com>
2023-10-13 15:18:03 +0530

    append child

--- dom4/dom4.js
+++ dom4/dom4.js
@@ -1,5 +1,5 @@
 var form =document.getElementById('addForm');
-var itemList=document.getElementById('items');
+var itemList=document.getElementById('items').value;
 
 
 // form submit event
@@ -10,5 +10,16 @@ form.addEventListener('submit',addItem);
 function addItem(e){
 // prevent Intial behaviour
 e.prevent.default();
-console.log(1);
+
+// get input value
+var newItem=document.getElementById("item");
+
+// create new li element
+var li=document.createElement('li');
+// add className
+li.className="list-group-item";
+// add text node with input value
+li.appendChild(document.createTextNode(newItem));
+
+itemList.appendChild(li);
 }
