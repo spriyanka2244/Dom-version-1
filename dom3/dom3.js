cd595854440be64e5b9fb055a6d371ca9f452c4d
spriyanka2244 <spriyanka2244@gmail.com>
2023-10-13 13:03:05 +0530

    but-text-bgcolor

--- dom3/dom3.js
+++ dom3/dom3.js
@@ -3,5 +3,7 @@ var button = document.getElementById('button').addEventListener
 ('click',buttonClick);
 
 function buttonClick(){
-    console.log('button clicked')
+    //console.log('button clicked')
+    document.getElementById('header-title').textContent="hello world"
+    document.querySelector('#main').style.backgroundColor="red"
 }
