5f6b8ec1c1b937482403cb317decac4f08928acf
spriyanka2244 <spriyanka2244@gmail.com>
2023-10-13 15:27:10 +0530

    delete button

--- dom4/dom4.js
+++ dom4/dom4.js
@@ -26,7 +26,11 @@ var delbut=document.createElement('button');
 // add class to del butn
 delbut.className='btn btn-danger btn-sm float-right delete';
 
+delbut.appendChild(document.createTextNode('X'));
 
+// append button to li
+li.appendChild(delbut);
 
+// append li to list
 itemList.appendChild(li);
 }
